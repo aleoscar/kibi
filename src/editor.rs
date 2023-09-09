76115675ecb77aac8ea17de5363f59c4a5075823
Alexander Jansson <alexander.oscar.jansson@gmail.com>
2023-09-09 23:18:05 +0200

    moved functions to terminal and add welcome message

--- src/editor.rs
+++ src/editor.rs
@@ -1,13 +1,13 @@
-use std::io::{self};
 use crate::Terminal;
-use crossterm::{self, execute, cursor, terminal::{ClearType, self}, event::{
+use std::io;
+use crossterm::{self, execute, cursor, event::{
     self,
     Event::Key,
     KeyCode::Char,
-    KeyEventKind,
-    KeyEvent,
-    KeyModifiers
+    KeyEventKind, KeyEvent, KeyModifiers,
 }};
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 pub struct Editor {
     should_quit: bool,
@@ -23,7 +23,6 @@ impl Editor {
     }
 
     pub fn run(&mut self) {
-        terminal::enable_raw_mode().ok();
 
         loop {
 
@@ -40,7 +39,50 @@ impl Editor {
         }
     }
         
-    fn handle_key_press(&mut self,key_event: &KeyEvent) {
+    fn refresh_screen(&self) {
+
+        //should it move to (1, 1) or (0, 0)? 
+        //I think (0, 0)
+        execute!(io::stdout(), cursor::Hide).unwrap();
+        Terminal::clear_screen();
+        Terminal::cursor_position(0, 0);
+        if self.should_quit {
+            Terminal::clear_screen();
+            println!("goodbye!")
+        } else {
+            self.draw_rows();
+        }
+
+        execute!(io::stdout(), cursor::Show).unwrap();
+        //bad error handling
+        Terminal::flush().ok();
+    }
+
+    fn draw_rows(&self) {
+        let height = self.terminal.size().height;
+        for row in 0..height - 1 {
+            Terminal::clear_current_line();
+            if row == height / 3 {
+                self.draw_welcome_message()
+            } else {
+                println!("~\r")
+            }
+        }
+    }
+
+    fn draw_welcome_message(&self) {
+        let mut welcome_message = format!("Kibi text edior -- version {VERSION}");
+        let width = self.terminal.size().width as usize;
+        let len = welcome_message.len();
+        let padding = width.saturating_sub(len) / 2;
+        let spaces = " ".repeat(padding.saturating_sub(1));
+        welcome_message = format!("~{}{}", spaces, welcome_message);
+        welcome_message.truncate(width);
+        println!("{}\r", welcome_message);
+
+    }
+
+    pub fn handle_key_press(&mut self, key_event: &KeyEvent) {
         match  key_event {
             KeyEvent {modifiers: KeyModifiers::CONTROL, code: Char('q'), ..} => {
                 self.should_quit = true;
@@ -55,24 +97,6 @@ impl Editor {
             }
 
             //_ => false  
-        }
-    }
-
-    fn refresh_screen(&self) {
-
-        //should it move to (1, 1) or (0, 0)?
-        execute!(io::stdout(), cursor::MoveTo(1, 1)).unwrap();
-        execute!(io::stdout(), terminal::Clear(ClearType::All)).unwrap();
-        if self.should_quit {
-            println!("goodbye!")
-        } else {
-            self.draw_rows();
-        }
-    }
-
-    fn draw_rows(&self) {
-        for _ in 0..self.terminal.size().height {
-            println!("~\r")
         }
     }
 }
--- src/terminal.rs
+++ src/terminal.rs
@@ -1,3 +1,7 @@
+use std::io::{self, Write};
+use crossterm::{terminal::{self, ClearType},
+    execute, cursor, 
+    };
 pub struct Size {
     pub width: u16,
     pub height: u16
@@ -5,22 +9,44 @@ pub struct Size {
 
 pub struct Terminal {
     size: Size,
+    _stdout: io::Result<()>,
 }
 
 impl Terminal {
     pub fn default() -> Result<Self, std::io::Error> {
-        let width = crossterm::terminal::size()?.0;
-        let height = crossterm::terminal::size()?.1;
+        let width = terminal::size()?.0;
+        let height = terminal::size()?.1;
 
         Ok(
             Self {size: Size {
                 width: width,
                 height: height
-            }}
+            },
+            //bad error handling
+            _stdout: terminal::enable_raw_mode(),
+        }
         )
     }
 
+
     pub fn size(&self) -> &Size {
         &self.size
+    }
+
+    pub fn clear_screen() {
+        //bad error handling
+        execute!(io::stdout(), terminal::Clear(ClearType::All)).unwrap();
+    }
+
+    pub fn clear_current_line() {
+        execute!(io::stdout(), terminal::Clear(ClearType::CurrentLine)).unwrap();
+    }
+
+    pub fn cursor_position(x: u16, y: u16) {
+        execute!(io::stdout(), cursor::MoveTo(x, y)).unwrap();
+    }
+
+    pub fn flush() -> Result<(), std::io::Error> {
+        io::stdout().flush()
     }
 }
