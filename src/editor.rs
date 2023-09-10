42bcda351a24eb8f4dc9eb428f2bb05683858a34
Alexander Jansson <alexander.oscar.jansson@gmail.com>
2023-09-10 12:51:05 +0200

    can now render text to the editor

--- src/editor.rs
+++ src/editor.rs
@@ -1,5 +1,6 @@
 use crate::Terminal;
 use crate::Document;
+use crate::Row;
 use std::io;
 use crossterm::{self, execute, cursor, event::{
     self,
@@ -29,7 +30,7 @@ impl Editor {
             should_quit: false,
             terminal: Terminal::default().expect("failed to initalize terminal"),
             cursor_position: Position::default(),
-            document: Document::default(),
+            document: Document::open(),
         }
     }
 
@@ -68,12 +69,22 @@ impl Editor {
         Terminal::flush().ok();
     }
 
+    fn draw_row(&self, row: &Row) {
+        let start = 0;
+        let end = self.terminal.size().width as usize;
+        let row = row.render(start, end);
+        println!("{}\r", row)
+    }
+
     fn draw_rows(&self) {
         let height = self.terminal.size().height;
-        for row in 0..height - 1 {
+        for terminal_row in 0..height - 1 {
             Terminal::clear_current_line();
-            if row == height / 3 {
-                self.draw_welcome_message()
+            if let Some(row) = self.document.row(terminal_row as usize) {
+                self.draw_row(row);
+            } else if self.document.is_empty() && 
+                terminal_row == height / 3 {
+                    self.draw_welcome_message()
             } else {
                 println!("~\r")
             }
